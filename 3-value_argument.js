fb6cd77cea4c36f8d96142e2286cd5027f490bd6
Hammuel <69243647+Hammuel15@users.noreply.github.com>
2025-05-25 08:29:47 +0100

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,3 +1,10 @@
-if (process.argv[2] === undefined) {
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined) {
   console.log("No argument");
+} else if (arg2 === undefined) {
+  console.log(`one argument "${arg1}"`);
+} else {
+  console.log(`2 arguments: "${arg1} ${arg2}"`);
 }
