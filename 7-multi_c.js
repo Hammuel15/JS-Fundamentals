788a7a78d54fef8529634b1055c2183b8dfcaa07
Hammuel <69243647+Hammuel15@users.noreply.github.com>
2025-05-25 09:45:52 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,9 @@
-let x = promt ('input your value ');
+const count = parseInt(process.argv[2]);
 
-if (isNaN(x)) {
+if (isNaN(count)) {
   console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  while (i < x) {
+  for (let i = 0; i < count; i++) {
     console.log('C is fun');
-    i++;
   }
 }
