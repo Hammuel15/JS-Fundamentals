6d6c997f6b4daf4ed6d4b65cd849b003a84c6239
Hammuel <69243647+Hammuel15@users.noreply.github.com>
2025-05-25 09:13:36 +0100

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+let x = promt ('input your value ');
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
